519d50a73724c9139b31ea092dd7efb435511828
arthurmarmed <arthurmarmed1234@gmail.com>
2025-10-21 11:00:11 -0300

    Ajuste do redirecionamento da página de registro

--- jornal/views.py
+++ jornal/views.py
@@ -63,7 +63,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('jornal:index')
     else:
         form = UserCreationForm()
     
